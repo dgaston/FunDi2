e1ef45080859c38c455b8dd40c38ab712bd3f846
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-06-01 16:02:09 -0300

    Import DDB-style configuration parser from run-time configurations

--- fundi/config.py
+++ fundi/config.py
@@ -8,3 +8,52 @@
 
 
 """
+
+import sys
+import ConfigParser
+from collections import defaultdict
+
+def configure_runtime(infile):
+    """Parse the configuration settings from a file
+    :param infile: input filename
+    :type infile: string.
+    :returns:  dict -- A configuration dictionary.
+    """
+
+    configuration = defaultdict()
+    config = ConfigParser.SafeConfigParser()
+    config.read(infile)
+
+    try:
+        config.options('settings')
+    except ConfigParser.NoSectionError:
+        sys.stderr.write("No section: settings in file\n")
+        sys.exit()
+
+    try:
+        config.options('resources')
+    except ConfigParser.NoSectionError:
+        sys.stderr.write("No section: resources in file\n")
+        sys.exit()
+
+    # Set all options specified in file
+    for option in config.options('settings'):
+            configuration[option] = config.get('settings', option)
+
+    for resource in config.options('resources'):
+            configuration[resource] = config.get('resources', resource)
+
+    # Configure individual tools
+    for section in config.sections():
+        if section != 'settings' and section != 'resources':
+            tool = section
+            options = config.options(tool)
+            tool_dict = dict()
+
+            # Set all specified options
+            for option in options:
+                tool_dict[option] = config.get(tool, option)
+
+            configuration[tool] = tool_dict
+
+    return configuration
