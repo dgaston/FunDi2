784aa916fcbd2b28b31d98e3fe39a5531b953c91
Dan Gaston <daniel.gaston@deaddriftbio.com>
2016-06-01 15:52:49 -0300

    Further skeleton for methods and wrappers

---
+++ fundi/config.py
@@ -0,0 +1,10 @@
+"""
+.. module:: config
+   :platform: Unix, OSX
+   :synopsis: A module for dealing with run-time configuration data
+   into additional formats.
+
+.. moduleauthor:: Daniel Gaston <daniel.gaston@dal.ca>
+
+
+"""
---
+++ fundi/fundi.py
@@ -0,0 +1,10 @@
+"""
+.. module:: raxml
+   :platform: Unix, OSX
+   :synopsis: A module containing all appropriate methods for fundi function
+   into additional formats.
+
+.. moduleauthor:: Daniel Gaston <daniel.gaston@dal.ca>
+
+
+"""
